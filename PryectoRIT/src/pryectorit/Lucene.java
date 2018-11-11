ef960c6e4a9e121b80f94a9e0ef3d6f5cbd91624
Unknown <jorcubas22@gmail.com>
2018-11-11 00:58:25 -0600

    Snowball Mejorado.

--- PryectoRIT/src/pryectorit/Lucene.java
+++ PryectoRIT/src/pryectorit/Lucene.java
@@ -5,8 +5,26 @@
  */
 package pryectorit;
 
+import java.awt.List;
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.lucene.analysis.Analyzer.TokenStreamComponents;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.StopFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.es.SpanishAnalyzer;
+import org.apache.lucene.analysis.snowball.SnowballFilter;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.StandardFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
@@ -17,53 +35,78 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.classic.Token;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
-import org.apache.lucene.store.
+import org.apache.lucene.util.Version;
+import org.tartarus.snowball.ext.EnglishStemmer;
+import org.tartarus.snowball.ext.LithuanianStemmer;
+import org.tartarus.snowball.ext.SpanishStemmer;
 /**
  *
  * @author AARON
  */
 public class Lucene {
-    /**
-     * @param args the command line arguments
-     */
-    
-    private StandardAnalyzer analyzer = new StandardAnalyzer();
-    private Directory index = new RAMDirectory();
-    
-    public Lucene() throws IOException{
-
-
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
-        IndexWriter w = new IndexWriter(index, config);
-        addDoc(w, "Lucene in Action", "193398817", "");
-        //addDoc(w, "Lucene for Dummies", "55320055Z");
-        // addDoc(w, "Managing Gigabytes", "55063554A");
-        //addDoc(w, "The Art of Computer Science", "9900333X");
-        w.close();
-    }
-    
-  /*  public static void main(String[] args) throws IOException, ParseException {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException, ParseException {
+        // 0. Specify the analyzer for tokenizing text.
+        //    The same analyzer should be used for indexing and searching
         StandardAnalyzer analyzer = new StandardAnalyzer();
+
+        // 1. create the index
         Directory index = new RAMDirectory();
 
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
-        IndexWriter w = new IndexWriter(index, config);
-        addDoc(w, "Lucene in Action", "193398817");
-        //addDoc(w, "Lucene for Dummies", "55320055Z");
-        // addDoc(w, "Managing Gigabytes", "55063554A");
-        //addDoc(w, "The Art of Computer Science", "9900333X");
-        w.close();
         
-         // 2. query
+        IndexWriter w = new IndexWriter(index, config);
+        String texto = "y chiquitos van y a comer";
+        String result = "";
+        TokenStream stream  = analyzer.tokenStream(null, new StringReader(texto));
+        stream.reset();
+        
+        
+    String[] stopwords = {"a", "acá", "ahí", "ajena", "ajenas", "ajeno", "ajenos", "al", "algo", "algún", "alguna", "algunas", "alguno", "algunos", "allá", "alli", "allí", "ambos", "ampleamos", "ante",
+    "antes", "aquel", "aquella", "aquellas", "aquello", "aquellos", "aqui", "aquí", "arriba", "asi", "atras", "aun", "aunque", "bajo", "bastante", "bien", "cabe", "cada", "casi",
+    "cierta", "ciertas", "cierto", "ciertos", "como", "cómo", "con", "conmigo", "conseguimos", "conseguir", "consigo", "consigue", "consiguen", "consigues", "contigo", "contra",
+    "cual", "cuales", "cualquier", "cualquiera", "cualquieras", "cuan", "cuán", "cuando", "cuanta", "cuánta", "cuantas", "cuántas", "cuanto", "cuánto", "cuantos", "cuántos", "de",
+    "dejar", "del", "demás", "demas", "demasiada", "demasiadas", "demasiado", "demasiados", "dentro", "desde", "donde", "dos", "el", "él", "ella", "ellas", "ello", "ellos", "empleais",
+    "emplean", "emplear", "empleas", "empleo", "en", "encima", "entonces", "entre", "era", "eramos", "eran", "eras", "eres", "es", "esa", "esas", "ese", "eso", "esos", "esta", "estaba",
+    "estado", "estais", "estamos", "estan", "estar", "estas", "este", "esto", "estos", "estoy", "etc", "fin", "fue", "fueron", "fui", "fuimos", "gueno", "ha", "hace", "haceis", "hacemos",
+    "hacen", "hacer", "haces", "hacia", "hago", "hasta", "incluso", "intenta", "intentais", "intentamos", "intentan", "intentar", "intentas", "intento", "ir", "jamás", "junto", "juntos",
+    "la", "largo", "las", "lo", "los", "mas", "más", "me", "menos", "mi", "mía", "mia", "mias", "mientras", "mio", "mío", "mios", "mis", "misma", "mismas", "mismo", "mismos", "modo",
+    "mucha", "muchas", "muchísima", "muchísimas", "muchísimo", "muchísimos", "mucho", "muchos", "muy", "nada", "ni", "ningun", "ninguna", "ningunas", "ninguno", "ningunos", "no", "nos",
+    "nosotras", "nosotros", "nuestra", "nuestras", "nuestro", "nuestros", "nunca", "os", "otra", "otras", "otro", "otros", "para", "parecer", "pero", "poca", "pocas", "poco", "pocos",
+    "podeis", "podemos", "poder", "podria", "podriais", "podriamos", "podrian", "podrias", "por", "por qué", "porque", "primero", "puede", "pueden", "puedo", "pues", "que", "qué", "querer",
+    "quien", "quién", "quienes", "quienesquiera", "quienquiera", "quiza", "quizas", "sabe", "sabeis", "sabemos", "saben", "saber", "sabes", "se", "segun", "ser", "si", "sí", "siempre",
+    "siendo", "sin", "sín", "sino", "so", "sobre", "sois", "solamente", "solo", "somos", "soy", "sr", "sra", "sres", "sta", "su", "sus", "suya", "suyas", "suyo", "suyos", "tal", "tales",
+    "también", "tambien", "tampoco", "tan", "tanta", "tantas", "tanto", "tantos", "te", "teneis", "tenemos", "tener", "tengo", "ti", "tiempo", "tiene", "tienen", "toda", "todas", "todo",
+    "todos", "tomar", "trabaja", "trabajais", "trabajamos", "trabajan", "trabajar", "trabajas", "trabajo", "tras", "tú", "tu", "tus", "tuya", "tuyo", "tuyos", "ultimo", "un", "una", "unas",
+    "uno", "unos", "usa", "usais", "usamos", "usan", "usar", "usas", "uso", "usted", "ustedes", "va", "vais", "valor", "vamos", "van", "varias", "varios", "vaya", "verdad", "verdadera",
+    "vosotras", "vosotros", "voy", "vuestra", "vuestras", "vuestro", "vuestros", "y", "ya", "yo"};
+ 
+
+        CharArraySet stopw = new CharArraySet(Arrays.asList(stopwords), true);
+        
+        //StopAnalyzer Stopanalyzer = new StopAnalyzer(stopw);
+        stream = new StopFilter(stream, stopw);
+        stream = new SnowballFilter(stream, new SpanishStemmer());
+        result = "";
+        while (stream.incrementToken()) {
+            result += stream.getAttribute(CharTermAttribute.class).toString();
+            result+=" ";
+            System.out.println(result);
+       }
+        stream.close();
+        addDoc(w, "Lucene in Action", "193398817");
+        addDoc(w, "Lucene for Dummies", "55320055Z");
+        addDoc(w, "Managing Gigabytes", "55063554A");
+        addDoc(w, "The Art of Computer Science", "9900333X");
+        w.close();
+
+        // 2. query
         String querystr = args.length > 0 ? args[0] : "lucene";
 
         // the "title" arg specifies the default field to use
@@ -88,51 +131,15 @@ public class Lucene {
         // reader can only be closed when there
         // is no need to access the documents any more.
         reader.close();
-    }*/
-    
-    public void parseQuery(String querystr) throws ParseException, IOException{
-        // the "title" arg specifies the default field to use
-        // when no field is explicitly specified in the query.
-        Query q = new QueryParser("title", analyzer).parse(querystr);
-        
-         // 3. search
-        int hitsPerPage = 10;
-        IndexReader reader = DirectoryReader.open(index);
-        IndexSearcher searcher = new IndexSearcher(reader);
-        TopDocs docs = searcher.search(q, hitsPerPage);
-        ScoreDoc[] hits = docs.scoreDocs;
-
-        // 4. display results
-        System.out.println("Found " + hits.length + " hits.");
-        for(int i=0;i<hits.length;++i) {
-            int docId = hits[i].doc;
-            Document d = searcher.doc(docId);
-            System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
-        }
-
-        // reader can only be closed when there
-        // is no need to access the documents any more.
-        reader.close();
     }
-    
-    public void addDoc(IndexWriter w, String title, String body, String a) throws IOException {
+
+    private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
         Document doc = new Document();
         doc.add(new TextField("title", title, Field.Store.YES));
-        doc.add(new TextField("body", body, Field.Store.YES));
-        doc.add(new TextField("a", title, Field.Store.YES));
-        doc.add(new TextField("h1", title, Field.Store.YES));
-        doc.add(new TextField("h2", title, Field.Store.YES));
-        doc.add(new TextField("h3", title, Field.Store.YES));
-        doc.add(new TextField("h4", title, Field.Store.YES));
-        doc.add(new TextField("h5", title, Field.Store.YES));
-        doc.add(new TextField("h6", title, Field.Store.YES));
-        doc.add(new TextField("h7", title, Field.Store.YES));
-        doc.add(new TextField("h8", title, Field.Store.YES));
-        doc.add(new TextField("h9", title, Field.Store.YES));
-        
-        //doc.add(new StringField("isbn", isbn, Field.Store.YES));
-        
-        
+
+        // use a string field for isbn because we don't want it tokenized
+        doc.add(new StringField("isbn", isbn, Field.Store.YES));
         w.addDocument(doc);
     }
+    
 }
